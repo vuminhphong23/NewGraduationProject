f12c959563c3bc6485f0e5c80f88502387c39bce
PhongVu <phongvuminh2003@gmail.com>
2025-06-21 21:08:31 +0700

    update spring boot

---
+++ src/main/java/GraduationProject/forumikaa/ForumikaaApplication.java
@@ -0,0 +1,13 @@
+package GraduationProject.forumikaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumikaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumikaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GraduationProject/forumikaa/ForumikaaApplicationTests.java
@@ -0,0 +1,13 @@
+package GraduationProject.forumikaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumikaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
